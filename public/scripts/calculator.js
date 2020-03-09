93824ac94ad32301a4902e367ebf994757b52e3f
victorcwyu <victorcwyu@gmail.com>
2020-03-09 23:52:28 +0000

    Updated class names, finished counter function.

--- public/scripts/calculator.js
+++ public/scripts/calculator.js
@@ -1,19 +1,38 @@
 $(document).ready(function () {
-  $(".add").click(function () {
+  $(".addCart").click(function () {
     event.preventDefault();
-    let i = parseInt($("#counter").text());
-    i++;
-    $("#counter").text(i + "X BUCKET OF FRIED CHICKEN");
-  });
-  $(".remove").click(function () {
-    event.preventDefault();
-    let i = parseInt($("#counter").text());
-    if (i <= 0) {
-      //some function to stop it/do nothing
-      i === 0;
-    } else {
-      i--;
-    };
-    $("#counter").text(i + " X BUCKET OF FRIED CHICKEN");
+    $(".newItem").append($(`<button class="add ui blue button" tabindex="0">+</button> <span id="counter">0</span> <button class="remove ui red button" tabindex="0">-</button>`));
+    $(".add").click(function () {
+      event.preventDefault();
+      let i = parseInt($("#counter").text());
+      i++;
+      $("#counter").text(i + "X BUCKET OF FRIED CHICKEN");
+    });
+    $(".remove").click(function () {
+      event.preventDefault();
+      let i = parseInt($("#counter").text());
+      if (i <= 0) {
+        //some function to stop it/do nothing
+        i === 0;
+      } else {
+        i--;
+      };
+      $("#counter").text(i + " X BUCKET OF FRIED CHICKEN");
+    });
   });
 });
+
+
+
+
+
+
+// function addOrder(orderName) ->
+//   Orderelement {
+//     <h4 class="ui dividing header">Order Summary</h4>
+//     <button class="add ui blue button" tabindex="0">+</button>
+//     <span id="counter">0</span>
+//     <button class="remove ui red button" tabindex="0">-</button>
+//   create outer div
+//   return the outer div
+// }
