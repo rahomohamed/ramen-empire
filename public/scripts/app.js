316c30f641196f07f913eae295cb117c49f434ca
rahomohamed <rahojmohamed@gmail.com>
2020-03-10 19:04:28 +0000

    added more comments on app.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -9,24 +9,21 @@ $(() => {
   // });;
 
   const createMenuItem = function(item) {
-    console.log('created menu item')
     return`  <article class = "menu">
     <div class="item-image">
     <img class="ui medium circular image" src=${item.image}>
   </div>
-<h4 class="ui dividing header">${item.price}</h4>
-<button class="add ui blue button" tabindex="0">Add</button>
-<button class="remove ui red button" tabindex="0">Remove</button>
-<h3>${item.name}</h3>
+  <h3>${item.name}</h3>
+  <h4 class="ui dividing header">${item.price}</h4>
+  <button class="add ui blue button" tabindex="0">Add</button>
+  <button class="remove ui red button" tabindex="0">Remove</button>
 <h4>${item.description}</h4>
   </article>`
 }
 
 // renders menu items
 const renderMenu = function(items) {
-  // $("#menu-container").empty();
   for (let item of items) {
-    console.log(item)
   $("#menu-container").append(createMenuItem(item));
   }
 }
@@ -34,8 +31,7 @@ const renderMenu = function(items) {
 const loadMenu = function() {
 $.ajax({
 url: '/api/menu',
-method: "GET",
-dataType: "JSON"
+method: "GET"
 })
 .then(response => {
 renderMenu(response);
