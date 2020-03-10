9a93efbf93ba5a411f0d080d4da5cb919b3fc0e8
rahomohamed <rahojmohamed@gmail.com>
2020-03-10 00:38:19 +0000

    changed default port to 3000

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 require('dotenv').config();
 
 // Web server config
-const PORT       = process.env.PORT || 8080;
+const PORT       = process.env.PORT || 3000;
 const ENV        = process.env.ENV || "development";
 const express    = require("express");
 const bodyParser = require("body-parser");
