25e2a4b853c3efd4e5a4f7fb4d6274d0c7a344e9
rahomohamed <rahojmohamed@gmail.com>
2020-03-10 00:37:55 +0000

    added routing files in to master

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -8,3 +8,26 @@ $(() => {
     }
   });;
 });
+$(() => {
+  // $.ajax({
+  //   method: "GET",
+  //   url: "/api/users"
+  // }).done((users) => {
+  //   for(user of users) {
+  //     $("<div>").text(user.name).appendTo($("body"));
+  //   }
+  // });;
+
+  const createMenuItem = function(item) {
+    console.log('created menu item')
+    return`  <article class = "menu">
+    <div class="item-image">
+    <img class="ui medium circular image" src=${item.image}>
+  </div>
+<h4 class="ui dividing header">${item.price}</h4>
+<button class="add ui blue button" tabindex="0">Add</button>
+<button class="remove ui red button" tabindex="0">Remove</button>
+<h3>${item.name}</h3>
+<h4>${item.description}</h4>
+  </article>`
+    }
