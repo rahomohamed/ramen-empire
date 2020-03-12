2d12ae9bac1aaebf6513697b3578843a0829b5d8
victorcwyu <victorcwyu@gmail.com>
2020-03-12 18:53:03 +0000

    Updated styling and menu database.

--- db/seeds/04_menu_item.sql
+++ db/seeds/04_menu_item.sql
@@ -1,5 +1,5 @@
 INSERT INTO menu_item (name, image, description, price, category_id)
-VALUES('Fried Chicken Meal', 'https://img.grouponcdn.com/deal/2ysg7QwS5tWBNELdphaezWnptbet/2y-792x475/v1/c700x420.jpg', 'marinated in Popeyes spicy chicken marinade, then hand-battered and breaded in our uniquely Southern crispy coating.', 12, '1'),
-('Chicken Sandwich', 'https://shawglobalnews.files.wordpress.com/2019/09/popeyes-chicken-e1567525805155.jpg?quality=70&strip=all', 'Crispy chicken marinated with onion, garlic, red and black peppers and served on an artisan bun with lettuce, tomatoes and a creamy mayo.', 11, '2'),
-('Chicken Tender Meal', 'https://assets.bwbx.io/images/users/iqjWHBFdfxIU/imSokNwqhfdk/v1/1000x-1.jpg', 'Handcrafted Spicy Tenders that pack a Louisiana punch in every bite. Round it out with a hot buttery Biscuit and one of our Signature Sides.', 13, '3');
+VALUES('Miso Ramen', 'https://img.grouponcdn.com/deal/2ysg7QwS5tWBNELdphaezWnptbet/2y-792x475/v1/c700x420.jpg', 'marinated in Popeyes spicy chicken marinade, then hand-battered and breaded in our uniquely Southern crispy coating.', 10, '1'),
+('Shoyu Ramen', 'https://shawglobalnews.files.wordpress.com/2019/09/popeyes-chicken-e1567525805155.jpg?quality=70&strip=all', 'Crispy chicken marinated with onion, garlic, red and black peppers and served on an artisan bun with lettuce, tomatoes and a creamy mayo.', 15, '2'),
+('House Special Ramen', 'https://assets.bwbx.io/images/users/iqjWHBFdfxIU/imSokNwqhfdk/v1/1000x-1.jpg', 'Handcrafted Spicy Tenders that pack a Louisiana punch in every bite. Round it out with a hot buttery Biscuit and one of our Signature Sides.', 20, '3');
 
