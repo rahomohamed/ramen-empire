79a976f9f3397d3281a9828a58626fee165edb93
Altair <altair.ioffe@gmail.com>
2020-03-12 23:50:28 +0000

    added first twilio function under /api/twilio.js

--- routes/twilio.js
+++ routes/twilio.js
@@ -1,5 +1,27 @@
 const express = require("express");
+const app = express();
 const router = express.Router();
+
+
+
+const twilio = require('twilio');
+const MessagingResponse = require('twilio').twiml.MessagingResponse;
+
+
+require('dotenv').config(); //to pull .env
+
+//nodemodules/twilio/lib/rest/Twilio.js
+
+// Load configuration information from system environment variables.
+const TWILIO_ACCOUNT_SID = process.env.TWILIO_ACCOUNT_SID,
+    TWILIO_AUTH_TOKEN = process.env.TWILIO_AUTH_TOKEN,
+    TWILIO_PHONE_NUMBER = process.env.TWILIO_PHONE_NUMBER,
+    CUSTOMER_PHONE_NUMBER= process.env.CUSTOMER_PHONE_NUMBER,
+    RESTAURANT_PHONE_NUMBER = process.env.RESTAURANT_PHONE_NUMBER;
+
+
+// Create an authenticated client to access the Twilio REST API
+const client = twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
 
 
 module.exports = db => {
@@ -7,12 +29,19 @@ module.exports = db => {
    router.post("/", (req, res) => {
 
 
-    console.log('BODY: ', req.body)
-    //SELECT order items & quantities, save to ONE STRING (for SMS)
-    //OR send order summary as string in req.body
-    //***Twilio logic goes here
+    console.log('BODY: ', req.body.address)
 
-    //can also pull address from req.body.address here if necessary
+      client.messages.create({
+        to: RESTAURANT_PHONE_NUMBER,
+        from: TWILIO_PHONE_NUMBER,
+        body: `New order: ${req.body.address}.\nPlease respond with ETA as numerical value in MINUTES.`
+      })
+      .then(function(message) {
+       // console.log('this is the then message: ', message);
+        res.redirect('/confirmed');
+      })
+      .catch(error => res.status(500).send(error));
+
 
   res.redirect("/confirmation");
   });
