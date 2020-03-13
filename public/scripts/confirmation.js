5e2e59ff23bbc32ad6f87902b1364f0575b146ab
Jeffrey <jeffrey.liu90@gmail.com>
2020-03-12 22:52:56 -0400

    Changed the map to show different methods of travel

--- public/scripts/confirmation.js
+++ public/scripts/confirmation.js
@@ -1,22 +1,30 @@
 $(document).ready(function() {
-  const replaceFunction = entry => {
-    return entry.replace(/ /g, "+");
-  };
-  console.log(googleApiKey);
-  $.ajax({
-    method: "GET",
-    url: "/api/address"
-  }).done(res => {
-    const newAddress = replaceFunction(res[0].address_name);
-    const map = `        <iframe
-    width="600"
-    height="450"
-    frameborder="0"
-    style="border:0"
-    src="https://www.google.com/maps/embed/v1/directions?key=${googleApiKey}&origin=${newAddress}&destination=Lighthouse+Labs+Toronto&avoid=tolls|highways"
-    allowfullscreen
-  ></iframe>`;
+  function setTransportationMethod(method) {
+    const method = localStorage.setItem("transportationMethod", method);
+  }
 
-    $(".google-api").append(map);
+  function reloadPage() {
+    window.reload();
+  }
+
+  console.log("my script has loaded");
+
+  $("#walking").click(event => {
+    event.preventDefault();
+    console.log("test");
+    setTransportationMethod("WALKING");
+    reloadPage();
+  });
+
+  $("#driving").click(event => {
+    event.preventDefault();
+    setTransportationMethod("DRIVING");
+    reloadPage();
+  });
+
+  $("#bicycling").click(event => {
+    event.preventDefault();
+    setTransportationMethod("BICYCLING");
+    reloadPage();
   });
 });
