d9335cc457a36d73780869a05fb23adbe021aacf
rahomohamed <rahojmohamed@gmail.com>
2020-03-13 05:30:40 +0000

    added confirmation page

--- public/scripts/confirmation.js
+++ public/scripts/confirmation.js
@@ -27,4 +27,28 @@ $(document).ready(function() {
     setTransportationMethod("BICYCLING");
     reloadPage();
   });
+  function generateRandomString() {
+
+    let result = "";
+    const characters = "0123456789";
+    const charactersLength = characters.length;
+
+    for (let i = 0; i < 6; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+  }
+
+  function createConfirmation() {
+      return `<h4>Your confirmatuion number is: ${generateRandomString()}</h4>
+    <p>Your order will be on the way shortly!</p>
+    <h4>It should arrive in approximately 10 minutes</h4>`
+    };
+
+    function confirmationPage() {
+    $("#confirmation").append(createConfirmation());
+      }
+
+  confirmationPage();
+
 });
